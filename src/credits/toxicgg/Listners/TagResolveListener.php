97fe00e7a1b02fdb16cad661e0bc0d9a2742d086
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-03-09 20:28:47 +0200

    test..

---
+++ src/credits/toxicgg/Listners/TagResolveListener.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types = 1);
+
+namespace credits\toxicgg\Listners;
+
+use credits\toxicgg\Main;
+use Ifera\ScoreHud\event\TagsResolveEvent;
+use pocketmine\event\Listener;
+use function count;
+use function explode;
+
+class TagResolveListener implements Listener{
+
+	/** @var Main */
+	private $plugin;
+
+	public function __construct(Main $plugin){
+		$this->plugin = $plugin;
+	}
+
+	public function onTagResolve(TagsResolveEvent $event){
+		$player = $event->getPlayer();
+		$tag = $event->getTag();
+		$tags = explode('.', $tag->getName(), 2);
+		$value = "";
+
+		if($tags[0] !== 'cscore' || count($tags) < 2){
+			return;
+		}
+
+		switch($tags[1]){
+			case "credits":
+				$value = $this->plugin->getCredits($player);
+				break;
+		}
+
+		$tag->setValue($value);
+	}
+}
