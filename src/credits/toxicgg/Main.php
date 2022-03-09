9d9d8af3dc1be004c0141d80eaea467c82fc0952
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-03-09 20:36:00 +0200

    Update Main.php

--- src/credits/toxicgg/Main.php
+++ src/credits/toxicgg/Main.php
@@ -1,1 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace credits\toxicgg;
+
+use Laith98Dev\Credits\Main as Credits;
+use credits\toxicgg\listners\EventListener;
+use credits\toxicgg\listners\TagResolveListener;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+  
+  /** @var Credits */
+	private $credits;
+  
+  protected function onEnable(): void{
+		      $this->credits = $this->getServer()->getPluginManager()->getPlugin("Credits");
+
+		      $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+		      $this->getServer()->getPluginManager()->registerEvents(new TagResolveListener($this), $this);
+	}
+  
+  public function getCredits(Player $player) : string{
+          $api = $this->getCredits($player);
+  }
+
 
