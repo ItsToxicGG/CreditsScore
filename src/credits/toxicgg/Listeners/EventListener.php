2ed8fff512b2045fa42788d5f3d80d94bb334f90
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-06-30 13:02:06 +0200

    Update and rename src/credits/toxicgg/Listners/EventListener.php to src/credits/toxicgg/Listeners/EventListener.php

---
+++ src/credits/toxicgg/Listeners/EventListener.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace credits\toxicgg\listeners;
+
+use credits\toxicgg\Main;
+use Ifera\ScoreHud\event\PlayerTagUpdateEvent;
+use Ifera\ScoreHud\scoreboard\ScoreTag;
+use pocketmine\event\Listener;
+use pocketmine\player\Player;
+
+class EventListener implements Listener {
+  
+  /** @var Main */
+	private $plugin;
+
+	public function __construct(Main $plugin){
+		$this->plugin = $plugin;
+	}
+ 
+  public function onCreditsChange(Player $player){ // not a real event
+                $username = $player->getName();
+    
+		if(is_null($username)){
+			return;
+		}
+
+		$player = $this->plugin->getServer()->getPlayerByPrefix($username);
+
+		if($player instanceof Player && $player->isOnline()){
+			(new PlayerTagUpdateEvent($player, new ScoreTag("credits", (string) $this->plugin->credits->getCredits())));
+		}
+  }
+}
--- src/credits/toxicgg/Listners/EventListener.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace credits\toxicgg\Listners;
-
-use pocketmine\event\Listener;
-
-class EventListener implements Listener{
-  // idk if i need this or now
-}
