f6b654e95f5754416ac20b4edf39c0b3f41abe2d
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-07-30 16:00:21 +0200

    change tag

--- src/credits/toxicgg/Listeners/EventListener.php
+++ src/credits/toxicgg/Listeners/EventListener.php
@@ -27,7 +27,7 @@ class EventListener implements Listener {
 		$player = $this->plugin->getServer()->getPlayerByPrefix($username);
 
 		if($player instanceof Player && $player->isOnline()){
-			(new PlayerTagUpdateEvent($player, new ScoreTag("credits", (string) $this->plugin->credits->getDataManager()->getCredits())));
+			(new PlayerTagUpdateEvent($player, new ScoreTag("player.credits", (string) $this->plugin->credits->getDataManager()->getCredits())));
 		}
   }
 }
